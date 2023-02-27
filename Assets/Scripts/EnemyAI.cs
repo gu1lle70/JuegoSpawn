cf57b6cee97f73e290f7bfa13a1f8ce0e969166d
DESKTOP-JR6KLRR\guillePC <guillem.salas@enti.cat>
2023-02-27 08:28:24 +0100

    juego

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3000;
+    [SerializeField] Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPosition = player.position - transform.position;
+        transform.Translate(newPosition * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("For Movement")]
+    [SerializeField] public float speed;
+    [SerializeField] Rigidbody2D playerRB;
+
+    [Header("For Shooring")]
+    [SerializeField] GameObject bullet;
+    [SerializeField] float fireRate;
+    private float nextFireTime;
+    private Rigidbody2D rb;
+    Vector2 input;
+    float angle;
+    public bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + input * speed * Time.fixedDeltaTime);
+        Shooting();
+    }
+    void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+
+        input.y = Input.GetAxis("Vertical");
+
+        if (input.x != 0 || input.y != 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+    }
+
+    void Shooting()
+    {
+        if (Input.GetKey(KeyCode.Space) && nextFireTime < Time.time)
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            nextFireTime = fireRate + Time.time;
+        }
+    }
+
+
+}
